7b34bb701198b5b582713e82915a50ffa03ee8e5
prisca.clio <prisca.clio@gmail.com>
2022-12-06 17:19:02 +0100

    cc

---
+++ question-6/terraform/main.tf
@@ -0,0 +1,13 @@
+resource "aws_kinesis_stream" "test_stream" {
+  ami           = "ami-02384a901b5df8024"
+  instance_type = "t3.micro"
+  key_name      = "prisca-clio-key"
+  tags = {
+    Name = "Etudiant"
+    Owner = "prisca.clio@etu.u-pec.fr"
+  }
+  
+  vpc_security_group_ids = ["${aws_security_group.default.id}"] 
+}
+
+# Default ec2 user is "ec2-user"
